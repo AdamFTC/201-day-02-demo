9adc275ab5007d5c19e3ca20f76cd98cd20ae114
Katherine Beame <katherine.beame@gmail.com>
2016-01-07 15:03:30 -0800

    Got the non-alert question to work, editted README

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 201-day-02-demo
-demo for 2nd day of class 
+Author: Adam Leonard
+Edited by: Katherine Beame
+Edited on: 1/7/16
+Function: A one question quiz about Sam's Cats
 Modified @ 10:39AM Pacific time.
--- app.js
+++ app.js
@@ -6,13 +6,17 @@ console.log('The user\'s name is ' + user);
 var answer1 = prompt(user + ', this is a yes/no question, so please answer with Y or N. Does Sam have 4 cats?');
 console.log('The user answered Question 1: ' + answer1);
 
+//question one
+//print out function rather than alert
+var response1 = document.getElementById('ref1');
 function question1() {
   if(answer1 === 'N') {
-    alert('Damn straight. Sam has 3 cats.');
+    response1.textContent = 'Damn straight. Sam has 3 cats.';
   }
   else {
-    alert('WRONG. YOU LOSE. HAHA.');
+    response1.textContent = 'WRONG. YOU LOSE. HAHA.';
 
   }
 }
+//call function
 question1();
